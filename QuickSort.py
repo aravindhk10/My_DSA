30bc9e93ca0d1ec7a0c7125fdb8cfce2206843d8
Aravind H Krishnan <72988073+aravindhk10@users.noreply.github.com>
2025-03-01 22:02:23 +0530

    Update QuickSort.py

--- QuickSort.py
+++ QuickSort.py
@@ -4,14 +4,29 @@ def quick(arr, low, high):
         quick(arr, low, p-1)
         quick(arr, p+1, high)
     return arr
+# def part(arr, low, high):
+#     l = low
+#     r = high
+#     pivot = arr[low]
+#     while l<=r:
+#         while arr[l]<=pivot and l<=high-1:
+#             l+=1
+#         while arr[r]>pivot and r>=low+1:
+#             r-=1
+#         if l<r:
+#             arr[l], arr[r] = arr[r], arr[l]
+#     arr[r], arr[low] = arr[low], arr[r]
+#     return r
+
+# more efficent
 def part(arr, low, high):
     l = low
     r = high
     pivot = arr[low]
     while l<=r:
-        while arr[l]<=pivot and l<=high-1:
+        while arr[l]<=pivot and l<high:
             l+=1
-        while arr[r]>pivot and r>=low+1:
+        while arr[r]>pivot:
             r-=1
         if l<r:
             arr[l], arr[r] = arr[r], arr[l]
